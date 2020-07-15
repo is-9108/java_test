348486691a7fdf9e19efa5043e48deafb1cdc7d6
is-9108 <syouta103010@gmail.com>
2020-07-15 21:59:33 +0900

    third commit

--- src/com/IS/Main.java
+++ src/com/IS/Main.java
@@ -2,14 +2,45 @@ package com.IS;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //maps
         Maps map = new Maps();
         String[][] stage = map.map1;
+
+        //player
+        Player player = new Player();
+        player.hitPoint = 10;
+        player.attack = 10;
+        player.heal = 5;
+        player.items = new String[]{
+                "1:",
+                "2:",
+                "3:",
+                "4:",
+                "5:",
+                "6:",
+                "7:",
+                "8:",
+                "9:",
+                "10:"
+        };
+        player.weapon = "なし";
+
+        //weapon
+        if (player.weapon == "なし") {
+            player.attack += 0;
+        } else if (player.weapon == "剣") {
+            player.attack += 10;
+        } else if (player.weapon == "大剣") {
+            player.attack += 20;
+            player.hitPoint -= 10;
+        }
+
 
         //enemy
         //slime
@@ -26,6 +57,15 @@ public class Main {
                 "   #####   "
         };
 
+        //playerPosition
+        int positonX = 0;
+        int positonY = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        stage[positonY][positonX] = "P";
 
         for(int i = 0; i < stage.length; i++) {
             for (int j = 0; j < stage[i].length; j++) {
@@ -34,16 +74,40 @@ public class Main {
             System.out.println("");
         }
 
-        int positonX = 0;
-        int positonY = 0;
+//
+
+        while (true){
+            System.out.println("コマンドを入力:");
+            String key = scanner.next();
+            switch (key){
+                case "w":
+                    positonY--;
+                    break;
+                case "a":
+                    positonX --;
+                    break;
+                case "s":
+                    positonY ++;
+                    break;
+                case "d":
+                    positonX ++;
+                    break;
+                default:
+                    return;
+            }
+            creatMap(stage,positonY,positonX);
+        }
 
     }
 
-    public void keyPressed(KeyEvent e){
-       switch (e.getKeyChar()){
-           case 'a':
-               System.out.println('a');
-               break;
-       }
+    public static void creatMap(String[][] map, int positionY, int positionX) {
+        String[][] stage = map;
+        stage[positionY][positionX] = "P";
+        for (int i = 0; i < stage.length; i++) {
+            for (int j = 0; j < stage[i].length; j++) {
+                System.out.print(stage[i][j]);
+            }
+            System.out.println("");
+        }
     }
 }
--- src/com/IS/Player.java
+++ src/com/IS/Player.java
@@ -1,4 +1,9 @@
 package com.IS;
 
 public class Player {
+    int hitPoint;
+    int attack;
+    String[] items = {};
+    int heal;
+    String weapon;
 }
